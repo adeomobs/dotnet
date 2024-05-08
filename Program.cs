17485e192817f68a45a7f74aa6d8a37cdac200b9
adeomobs <adeomobs@hotmail.com>
2024-05-08 14:30:58 +0000

    COnfig edit test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your nick name?");
+        Console.WriteLine("What is your name?");
         var name = Console.ReadLine();
         var currentDate = DateTime.Now;
         Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
--- obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcad7fd866e895cacf6f5f0261d9d62ff0f81b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ad36828b558bce985625078af095b677f2b789")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
